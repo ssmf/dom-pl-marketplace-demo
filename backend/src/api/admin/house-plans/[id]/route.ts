e801c9d39202564bb6acf0d59db1a6042a99ed76
ikuniewiczWaybetter <i.kuniewicz@waybetter.pl>
2026-03-25 11:57:01 +0100

    create validator and middleware to check house plan types

--- backend/src/api/admin/house-plans/[id]/route.ts
+++ backend/src/api/admin/house-plans/[id]/route.ts
@@ -1,15 +1,19 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
 import { HOUSE_PLAN_MODULE } from "../../../../modules/house_plan"
 import HousePlanModuleService from "../../../../modules/house_plan/service"
+import { UpdateHousePlanSchema } from "../validators"
 
-export async function POST(req: MedusaRequest, res: MedusaResponse) {
+export async function POST(
+  req: MedusaRequest<UpdateHousePlanSchema>,
+  res: MedusaResponse
+) {
   const { id } = req.params
   const housePlanService: HousePlanModuleService =
     req.scope.resolve(HOUSE_PLAN_MODULE)
 
   const house_plan = await housePlanService.updateHousePlans({
     id,
-    ...(req.body as object),
+    ...req.validatedBody,
   })
 
   res.json({ house_plan })
